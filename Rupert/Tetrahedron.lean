75541342c9ba217c9383ef249748d963338d433e
Will Bradley <68962413+a2435191@users.noreply.github.com>
2025-09-19 04:28:54 -0500

    Avoid unnecessary tactic copy-pastes

--- Rupert/Tetrahedron.lean
+++ Rupert/Tetrahedron.lean
@@ -42,46 +42,25 @@ theorem rupert : IsRupert vertices := by
     refine Convex.ball_in_hull_of_corners_in_hull hε₀ ?_ ?_ ?_ ?_ <;>
       apply mem_convexHull_iff_exists_fintype.mpr <;>
       use Fin 4, inferInstance
-    · use ![14470757879961/43300505182000,
+    <;> [
+      use ![14470757879961/43300505182000,
             14426795957911/43300505182000,
             0,
-            900184459008/2706281573875]
-      use fun i ↦ proj_xy (outer_rot *ᵥ vertices i)
-      refine ⟨?_, ?_, ?_, ?_⟩
-      · intro i; fin_cases i <;> norm_num
-      · simp [Fin.sum_univ_four]; norm_num
-      · exact fun i ↦ ⟨i, rfl⟩
-      · simp only [proj_xy, outer_rot, matrix_of_quat, outer_quat, vertices, Fin.sum_univ_four,
-                   ε₀, matrix_simps]
-        norm_num
-    · use ![72265247417243/216502525910000,
+            900184459008/2706281573875];
+      use ![72265247417243/216502525910000,
             72310753372299/216502525910000,
             0,
-            35963262560229/108251262955000]
-      use fun i ↦ proj_xy (outer_rot *ᵥ vertices i)
-      refine ⟨?_, ?_, ?_, ?_⟩
-      · intro i; fin_cases i <;> norm_num
-      · simp [Fin.sum_univ_four]; norm_num
-      · exact fun i ↦ ⟨i, rfl⟩
-      · simp only [proj_xy, outer_rot, matrix_of_quat, outer_quat, vertices, Fin.sum_univ_four,
-                   ε₀, matrix_simps]
-        norm_num
-    · use ![14483649997239/43300505182000,
+            35963262560229/108251262955000];
+      use ![14483649997239/43300505182000,
             14462186725689/43300505182000,
             0,
-            897166778692/2706281573875]
-      use fun i ↦ proj_xy (outer_rot *ᵥ vertices i)
-      refine ⟨?_, ?_, ?_, ?_⟩
-      · intro i; fin_cases i <;> norm_num
-      · simp [Fin.sum_univ_four]; norm_num
-      · exact fun i ↦ ⟨i, rfl⟩
-      · simp only [proj_xy, outer_rot, matrix_of_quat, outer_quat, vertices, Fin.sum_univ_four,
-                   ε₀, matrix_simps]
-        norm_num
-    · use ![72506791968757/216502525910000,
+            897166778692/2706281573875];
+      use ![72506791968757/216502525910000,
             72134160045701/216502525910000,
             0,
             35930786947771/108251262955000]
+    ]
+    all_goals
       use fun i ↦ proj_xy (outer_rot *ᵥ vertices i)
       refine ⟨?_, ?_, ?_, ?_⟩
       · intro i; fin_cases i <;> norm_num
@@ -100,55 +79,25 @@ theorem rupert : IsRupert vertices := by
   fin_cases y <;>
     simp only [vertices, Fin.reduceFinMk, Matrix.cons_val] at hy <;>
     use Fin 4, inferInstance
-  · use ![10743981448378145233579223/2255005124571996596714809125,
+  <;> [
+    use ![10743981448378145233579223/2255005124571996596714809125,
           64386129031453492435586819/13530030747431979580288854750,
           0,
-          13401180729710257216451792593/13530030747431979580288854750]
-    use fun i ↦ (1 - ε₁) • (proj_xy (outer_rot *ᵥ vertices i))
-    refine ⟨?_, ?_, ?_, ?_⟩
-    · intro i; fin_cases i <;> norm_num
-    · simp only [Fin.sum_univ_four, matrix_simps]; norm_num
-    · exact fun i ↦ ⟨proj_xy (outer_rot *ᵥ vertices i), by simp [outer_shadow]⟩
-    · rw [←hy]
-      simp only [proj_xy, outer_rot, matrix_of_quat, outer_quat, vertices, Fin.sum_univ_four,
-                 inner_offset, inner_rot, inner_quat, ε₁, matrix_simps]
-      rw [Matrix.smul_vec2, Matrix.smul_vec2, Matrix.smul_vec2,
-          Matrix.vec2_add, Matrix.vec2_add, Matrix.vec2_add]
-      norm_num
-  · use ![5556134647331086902480487669/6765015373715989790144427375,
+          13401180729710257216451792593/13530030747431979580288854750];
+    use ![5556134647331086902480487669/6765015373715989790144427375,
           2352935973121235655284086819/13530030747431979580288854750,
           0,
-          21608493216190040014597531/4510010249143993193429618250]
-    use fun i ↦ (1 - ε₁) • (proj_xy (outer_rot *ᵥ vertices i))
-    refine ⟨?_, ?_, ?_, ?_⟩
-    · intro i; fin_cases i <;> norm_num
-    · simp only [Fin.sum_univ_four, matrix_simps]; norm_num
-    · exact fun i ↦ ⟨proj_xy (outer_rot *ᵥ vertices i), by simp [outer_shadow]⟩
-    · rw [←hy]
-      simp only [proj_xy, outer_rot, matrix_of_quat, outer_quat, vertices, Fin.sum_univ_four,
-                 inner_offset, inner_rot, inner_quat, ε₁, matrix_simps]
-      rw [Matrix.smul_vec2, Matrix.smul_vec2, Matrix.smul_vec2,
-          Matrix.vec2_add, Matrix.vec2_add, Matrix.vec2_add]
-      norm_num
-  · use ![4608766371145456006819667/966430767673712827163489625,
+          21608493216190040014597531/4510010249143993193429618250];
+    use ![4608766371145456006819667/966430767673712827163489625,
           1914434962235023371784298117/1932861535347425654326979250,
           0,
-          3069680123370456843013933/644287178449141884775659750]
-    use fun i ↦ (1 - ε₁) • (proj_xy (outer_rot *ᵥ vertices i))
-    refine ⟨?_, ?_, ?_, ?_⟩
-    · intro i; fin_cases i <;> norm_num
-    · simp only [Fin.sum_univ_four, matrix_simps]; norm_num
-    · exact fun i ↦ ⟨proj_xy (outer_rot *ᵥ vertices i), by simp [outer_shadow]⟩
-    · rw [←hy]
-      simp only [proj_xy, outer_rot, matrix_of_quat, outer_quat, vertices, Fin.sum_univ_four,
-                 inner_offset, inner_rot, inner_quat, ε₁, matrix_simps]
-      rw [Matrix.smul_vec2, Matrix.smul_vec2, Matrix.smul_vec2,
-          Matrix.vec2_add, Matrix.vec2_add, Matrix.vec2_add]
-      norm_num
-  · use ![5556788588822333700340487669/6765015373715989790144427375,
+          3069680123370456843013933/644287178449141884775659750];
+    use ![5556788588822333700340487669/6765015373715989790144427375,
           64569206997427958451586819/13530030747431979580288854750,
           0,
           2351884362789884221156292593/13530030747431979580288854750]
+  ]
+  all_goals
     use fun i ↦ (1 - ε₁) • (proj_xy (outer_rot *ᵥ vertices i))
     refine ⟨?_, ?_, ?_, ?_⟩
     · intro i; fin_cases i <;> norm_num
@@ -158,5 +107,5 @@ theorem rupert : IsRupert vertices := by
       simp only [proj_xy, outer_rot, matrix_of_quat, outer_quat, vertices, Fin.sum_univ_four,
                  inner_offset, inner_rot, inner_quat, ε₁, matrix_simps]
       rw [Matrix.smul_vec2, Matrix.smul_vec2, Matrix.smul_vec2,
-          Matrix.vec2_add,Matrix.vec2_add, Matrix.vec2_add]
+          Matrix.vec2_add, Matrix.vec2_add, Matrix.vec2_add]
       norm_num
