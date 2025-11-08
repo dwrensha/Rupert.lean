fb66fae267514a1433a847ad1d18b14035cb033c
David Renshaw <dwrenshaw@gmail.com>
2025-11-08 11:09:16 -0500

    update snub cube and icosahedron to use WithLp notation

--- Rupert/SnubCube.lean
+++ Rupert/SnubCube.lean
@@ -9,35 +9,35 @@ noncomputable def trib : ℝ :=
   (1 + cbrt (19 + 3 * √33) + cbrt (19 - 3 * √33) ) / 3
 
 noncomputable def snub_cube : Fin 24 → ℝ³ :=
- ![ ![      -1,  1/trib,    trib],
-    ![       1, -1/trib,    trib],
-    ![       1,  1/trib,   -trib],
-    ![      -1, -1/trib,   -trib],
+ ![ !₂[      -1,  1/trib,    trib],
+    !₂[       1, -1/trib,    trib],
+    !₂[       1,  1/trib,   -trib],
+    !₂[      -1, -1/trib,   -trib],
 
-    ![ -1/trib,    trib,       1],
-    ![  1/trib,   -trib,       1],
-    ![  1/trib,    trib,      -1],
-    ![ -1/trib,   -trib,      -1],
+    !₂[ -1/trib,    trib,       1],
+    !₂[  1/trib,   -trib,       1],
+    !₂[  1/trib,    trib,      -1],
+    !₂[ -1/trib,   -trib,      -1],
 
-    ![   -trib,       1,  1/trib],
-    ![    trib,      -1,  1/trib],
-    ![    trib,       1, -1/trib],
-    ![   -trib,      -1, -1/trib],
+    !₂[   -trib,       1,  1/trib],
+    !₂[    trib,      -1,  1/trib],
+    !₂[    trib,       1, -1/trib],
+    !₂[   -trib,      -1, -1/trib],
 
-    ![       1,   -trib, -1/trib],
-    ![      -1,    trib, -1/trib],
-    ![      -1,   -trib,  1/trib],
-    ![      -1,    trib,  1/trib],
+    !₂[       1,   -trib, -1/trib],
+    !₂[      -1,    trib, -1/trib],
+    !₂[      -1,   -trib,  1/trib],
+    !₂[      -1,    trib,  1/trib],
 
-    ![  1/trib,      -1,   -trib],
-    ![ -1/trib,       1,   -trib],
-    ![ -1/trib,      -1,    trib],
-    ![  1/trib,       1,    trib],
+    !₂[  1/trib,      -1,   -trib],
+    !₂[ -1/trib,       1,   -trib],
+    !₂[ -1/trib,      -1,    trib],
+    !₂[  1/trib,       1,    trib],
 
-    ![     trib, -1/trib,     -1],
-    ![    -trib,  1/trib,     -1],
-    ![    -trib, -1/trib,      1],
-    ![     trib,  1/trib,      1]
+    !₂[     trib, -1/trib,     -1],
+    !₂[    -trib,  1/trib,     -1],
+    !₂[    -trib, -1/trib,      1],
+    !₂[     trib,  1/trib,      1]
   ]
 
 proof_wanted rupert : ¬ IsRupert snub_cube
